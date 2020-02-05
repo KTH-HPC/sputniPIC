aa9cb257ca55ebb9cc38cee7e114f2465a22b07e
steven <wdchien@kth.se>
2020-02-05 09:17:57 +0100

    fixed comment on precision

--- include/PrecisionTypes.h
+++ include/PrecisionTypes.h
@@ -2,11 +2,11 @@
 #define PRECISIONTYPES_H
 
 // precision for particle quanties (position, velocity, ...): choose float or
-// float
+// double
 typedef float FPpart;
-// precision for field quantities (Ex, Bx, ...): choose float or float
+// precision for field quantities (Ex, Bx, ...): choose float or double
 typedef float FPfield;
-// precision for interpolated quanties (Ex, Bx, ...): choose float or float
+// precision for interpolated quanties (Ex, Bx, ...): choose float or double
 typedef float FPinterp;
 
 #endif
